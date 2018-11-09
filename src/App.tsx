164ceac5069f4fd72f19e2f834673e49557e385b
João Carlos Fernandes Neto <wysrar@gmail.com>
2018-11-09 15:42:34 -0500

    v0.1

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">fdfdfdfdf</h1>
+        </header>
+        <p className="App-intro">
+          To get started,<code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
